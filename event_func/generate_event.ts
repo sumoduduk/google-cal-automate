3879581938f19a3ecf7721f3ff6d5c9645d56159
sumoduduk <sumoduduk13@gmail.com>
2024-05-08 15:31:40 +0700

    fix: add function to set date

--- event_func/generate_event.ts
+++ event_func/generate_event.ts
@@ -1,66 +1,51 @@
-import { EventType } from "../lib/type-cal";
+import { EventType, ScheduleFeedT } from "../lib/type-cal";
 import { getTimeFormat } from "../lib/util-time";
 import fs from "fs";
 
-export function genEvent() {
-  const data = fs.readFileSync("event-tomorrow.json", "utf8");
-  let time_schedules = JSON.parse(data);
+function splitHourMinute(time_str: string) {
+  const split_time = time_str.split(":");
+  return { hour: parseInt(split_time[0]), minute: parseInt(split_time[1]) };
+}
 
-  function splitHourMinute(time_str: string) {
-    const split_time = time_str.split(":");
-    return { hour: parseInt(split_time[0]), minute: parseInt(split_time[1]) };
-  }
+export function genEvent() {
+  const data = fs.readFileSync("ev_schedule.json", "utf8");
+  let time_schedules = JSON.parse(data) as Array<ScheduleFeedT>;
 
   const schedules_arr: Array<EventType> = [];
 
-  for (let schedule of time_schedules) {
-    let split_schedule = schedule.split("-");
-    let split_summary: Array<string> =
-      split_schedule[split_schedule.length - 1].split("/");
+  for (let elem of time_schedules) {
+    const date_event = elem.date;
 
-    let start = splitHourMinute(split_schedule[0]);
-    let end = splitHourMinute(split_schedule[1]);
+    for (let schedule_event of elem.schedule) {
+      const split_schedule = schedule_event.split("&?");
 
-    let summary = split_summary[0];
-    let description = split_summary[1];
+      let start = splitHourMinute(split_schedule[0]);
+      let end = splitHourMinute(split_schedule[1]);
 
-    let event: EventType = {
-      summary: summary.trim(),
-      description: description.trim(),
-      start: {
-        dateTime: getTimeFormat(1, start.hour, start.minute),
-        timeZone: "Asia/Jakarta",
-      },
-      end: {
-        dateTime: getTimeFormat(1, end.hour, end.minute),
-        timeZone: "Asia/Jakarta",
-      },
-      reminders: {
-        useDefault: false,
-        overrides: [
-          {
-            method: "popup", // Use 'popup' for phone notification (platform-dependent)
-            minutes: 10, // Send notification 10 minutes before
-          },
-        ],
-      },
-    };
+      let event: EventType = {
+        summary: split_schedule[2].trim(),
+        description: split_schedule[3].trim(),
+        start: {
+          dateTime: getTimeFormat(date_event, start.hour, start.minute),
+          timeZone: "Asia/Jakarta",
+        },
+        end: {
+          dateTime: getTimeFormat(date_event, end.hour, end.minute),
+          timeZone: "Asia/Jakarta",
+        },
+        reminders: {
+          useDefault: false,
+          overrides: [
+            {
+              method: "popup",
+              minutes: 10,
+            },
+          ],
+        },
+      };
 
-    schedules_arr.push(event);
+      schedules_arr.push(event);
+    }
   }
-
   return schedules_arr;
 }
-
-//   {
-//   description: "Test Description",
-//   summary: "test summary ??",
-//   start: {
-//     dateTime: start_time,
-//     timeZone: TIMEZONE,
-//   },
-//   end: {
-//     dateTime: end_time,
-//     timeZone: TIMEZONE,
-//   },
-// };
